c25bbfb5695fe2ae3255863ae0e8658a93a1651d
Scott McKay <skottmckay@gmail.com>
2019-12-27 11:09:57 +1000

    Add linux stacktrace to debug exception

--- onnxruntime/core/platform/posix/stacktrace.cc
+++ onnxruntime/core/platform/posix/stacktrace.cc
@@ -3,10 +3,36 @@
 
 #include "core/common/common.h"
 
+#include <execinfo.h>
+#include <vector>
+
 namespace onnxruntime {
 
 std::vector<std::string> GetStackTrace() {
-  return {"<stacktrace not implemented>"};
-}
+  constexpr int kCallstackLimit = 64;  // Maximum depth of callstack
 
+  void *array[kCallstackLimit];
+  char **strings = nullptr;
+
+  size_t size = backtrace (array, kCallstackLimit);
+  strings = backtrace_symbols (array, size);
+
+  std::vector<std::string> stack;
+  stack.reserve(size);
+
+  // hide GetStackTrace so the output starts with the 'real' location
+  constexpr size_t frames_to_skip = 1;
+
+  // we generally want to skip the first frame, but if something weird is going on (e.g. code coverage is
+  // running) and we only have 1 frame, output it so there's at least something that may be meaningful
+  const size_t start_frame = size > frames_to_skip ? frames_to_skip : 0;
+
+  for (size_t i = start_frame; i < size; i++) {
+    stack.push_back(strings[i]);
+  }
+
+  free(strings);
+
+  return stack;
+}
 }  // namespace onnxruntime
